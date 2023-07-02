0608c3a83d36998eacf7d37cea86d9ab8bd10430
wjd47 <wjd4782@naver.com>
2023-07-02 23:25:17 +0900

    2023-07-02

---
+++ springboot-developer/src/main/java/me/jeonghyeon/springbootdeveloper/TestController.java
@@ -0,0 +1,13 @@
+package me.jeonghyeon.springbootdeveloper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "헬로 월드";
+    }
+}
